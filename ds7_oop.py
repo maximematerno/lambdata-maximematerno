e526f313dffcc4ede01c5431c74767ca1f06da30
--global <--global>
2019-10-01 13:11:16 -0700

    oop

---
+++ ds7_oop.py
@@ -0,0 +1,34 @@
+"""
+Classes to represent games
+"""
+​
+class Game:
+    """ General representation of an abstract game"""
+​
+    fun_level = 5
+​
+    def __init__(self, player1="Stefano", player2="Rudy"):
+        self.rounds = 2
+        self.steps = 5
+        self.player1 = player1
+        self.player2 = player2
+​
+    def print_players(self):
+        """ Print game players"""
+        print('{} is playing {}'.format(self.player1, self.player2))
+​
+    def add_round(self):
+        """Increment rounds by 1"""
+        self.rounds += 1
+​
+class Tic(Game):
+    """
+    Tictactoe subclass of game
+    """
+​
+    def __init__(self,player1='Anika', player2='Hira'):
+        super().__init__(player1, player2)
+        self.rounds = 3
+​
+    def print_players(self):
+        print('{} is playing TIC TAC TOE with {}'.format(self.player1, self.player2))
--- null.py
+++ null.py
@@ -3,9 +3,15 @@ import pandas as pd
 import numpy as np
 
 
-def null(df):
+class Null_checker:
+
+	def __init__(self, df = df):
+        self.df = 0
+
+	def null(self):
     
-	if df.isnull().values.any():
-		return True, df.isnull().sum()
-	else:
-        return False, df.isnull().sum()
+		if df.isnull().values.any():
+			return True, df.isnull().sum()
+		else:
+			return False, df.isnull().sum()
+
