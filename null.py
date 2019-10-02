4e632c70a2ccaf5ef7a4ce2f41dc1cda4b2cbe95
--global <--global>
2019-10-02 00:34:55 -0700

    autopep8

--- null.py
+++ null.py
@@ -3,16 +3,14 @@ import pandas as pd
 import numpy as np
 
 
-
 class Null_checker():
 
-	def __init__(self, df):
-		self.df = pd.DataFrame()
+    def __init__(self, df):
+        self.df = pd.DataFrame()
 
-	def null(self):
-    
-		if df.isnull().values.any():
-			return True, df.isnull().sum()
-		else:
-			return False, df.isnull().sum()
+    def null(self):
 
+        if df.isnull().values.any():
+            return True, df.isnull().sum()
+        else:
+            return False, df.isnull().sum()
